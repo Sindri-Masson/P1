376b28063645044825c9428aca069660351649ab
Sindri-Masson <sindrim19@ru.is>
2023-01-30 14:11:50 +0000

    Fixed parseports to return ints

--- port_scanner.go
+++ port_scanner.go
@@ -7,8 +7,9 @@ import (
 	"strings"
 )
 
-func parse_ports(ports string) []string {
+func parse_ports(ports string) []int {
 	var ports_list []string = strings.Split(ports, ",");
+	var ports_list_int []int;
 	for i := 0; i < len(ports_list); i++ {
 		if strings.Contains(ports_list[i], "-") {
 			var start_end []string = strings.Split(ports_list[i], "-");
@@ -16,14 +17,35 @@ func parse_ports(ports string) []string {
 			var start, start_err = strconv.Atoi(strStart);
 			var end, end_err = strconv.Atoi(start_end[1]);
 			ports_list = append(ports_list[:i], ports_list[i+1:]...);
-			fmt.Println(start_err, end_err)
+			if start_err != nil || end_err != nil {
+				fmt.Println("Error parsing ports")
+				os.Exit(1);
+			}
 			for j := start; j <= end; j++ {
-				ports_list = append(ports_list, strconv.Itoa(j));
+				ports_list_int = append(ports_list_int, j);
 			}
 		}
+		var port, err = strconv.Atoi(ports_list[i]);
+		if err != nil {
+			fmt.Println("Error parsing ports")
+				os.Exit(1);
+		}
+		ports_list_int = append(ports_list_int, port);
 	}
-	return ports_list
+	return ports_list_int
 }
+/* func parse_ips(ips []string) []int {
+	var ips_list []int;
+	for i := 0; i < len(ips); i++ {
+		var port, err = strconv.Atoi(ips[i]);
+		if err != nil {
+			fmt.Println("Error parsing ips")
+			os.Exit(1);
+		}
+		ips_list = append(ips_list, port);
+	}
+	return ips_list
+} */
 
 func main() {
 	// Get the IP address and port range from the command line
@@ -33,7 +55,7 @@ func main() {
 	}
 	//print out the arguments
 	var ports string = os.Args[1];
-	var ports_list []string = parse_ports(ports);
+	var ports_list []int = parse_ports(ports);
 	var ips []string = os.Args[2:len(os.Args)];
 
 	fmt.Println(ports)
